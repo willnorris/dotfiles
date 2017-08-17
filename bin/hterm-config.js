c53cc18a489a20c3a8e6b8eb42bbe7d6eda5530d
Will Norris <will@willnorris.com>
2017-08-17 05:55:50 +0000

    hterm: enable desktop notification bell

--- bin/hterm-config.js
+++ bin/hterm-config.js
@@ -66,6 +66,7 @@ for (name in htermProfiles) {
     term_.prefs_.set('cursor-blink', true);
     term_.prefs_.set('cursor-blink-cycle', [800, 500]);
     term_.prefs_.set('scrollbar-visible', '');
+    term_.prefs_.set('desktop-notification-bell', true);
 };
 
 // restore current profile
