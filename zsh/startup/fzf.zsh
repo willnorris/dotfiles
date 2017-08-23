1b331659b0c3e3b386968bedb39f5910d624b0df
Will Norris <will@willnorris.com>
2017-08-23 05:12:55 +0000

    fzf: improve UI and key bindings
    - add preview for file browsing
    - add key bindings for paging through file list and preview

--- zsh/startup/fzf.zsh
+++ zsh/startup/fzf.zsh
@@ -5,10 +5,15 @@ bindkey -v
 
 if (( $+commands[fzf] )); then
   export FZF_DEFAULT_OPTS="
-  --color 16,fg+:-1,hl:1,hl+:1,info:2,marker:1 --no-bold
+  --color 16,fg+:-1,hl:1,hl+:1,info:2,marker:1 --no-bold --height '40%'
   --preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C -L 1 -F {}) 2> /dev/null | head -200'
-  --bind '?:toggle-preview'
+  --bind '?:toggle-preview,alt-j:preview-down,alt-k:preview-up'
+  --bind 'alt-space:preview-page-down,alt-f:preview-page-down,alt-b:preview-page-up'
+  --bind 'ctrl-f:page-down,ctrl-b:page-up'
   "
+  export FZF_CTRL_T_OPTS="--no-reverse"
+  export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+
   export FZF_TMUX=1
 
   if [[ -f "$FZF_HOME/shell/completion.zsh" ]]; then
