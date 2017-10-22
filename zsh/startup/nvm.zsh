966a7ba99fb71374fd2b507fe1d300eef2fb1434
Will Norris <will@willnorris.com>
2017-10-22 17:46:17 +0000

    zsh: refactor nvm config
    - separate nvm.zshenv from nvm.zsh.
    - load nvm if present.  This was previously disabled because it was
    pretty slow.  Things seem to be faster now, so trying this again.

---
+++ zsh/startup/nvm.zsh
@@ -0,0 +1,9 @@
+# vim: ft=zsh
+
+if [[ -e $NVM_DIR/nvm.sh ]]; then
+  source "$NVM_DIR/nvm.sh"
+fi
+
+if [[ -e $NVM_DIR/bash_completion ]]; then
+  source "$NVM_DIR/bash_completion"
+fi
--- zsh/startup/nvm.zshenv
+++ zsh/startup/nvm.zshenv
@@ -3,7 +3,3 @@
 if (( ! $+NVM_DIR )) && [[ -d $HOME/.nvm ]]; then
   export NVM_DIR=$HOME/.nvm
 fi
-
-if [[ -e $NVM_DIR/nvm.sh ]]; then
-  source "$NVM_DIR/nvm.sh"
-fi
