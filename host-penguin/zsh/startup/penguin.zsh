1c72af98f945ff133ff54e35f14a74d09c105d83
Will Norris <will@willnorris.com>
2019-05-02 20:22:27 -0700

    add aliases for running linux apps in crostini
    use sommelier flags to fix ui scaling

--- host-penguin/zsh/startup/penguin.zsh
+++ host-penguin/zsh/startup/penguin.zsh
@@ -5,3 +5,7 @@ fi
 if [[ "$TERM" == "xterm" ]]; then
   export TERM="xterm-256color"
 fi
+
+alias gimp='sommelier -X --scale=0.4 --dpi=96 /usr/bin/gimp &'
+alias inkscape='sommelier -X --scale=0.4 --dpi=96 /usr/bin/inkscape &'
+alias code='sommelier -X --scale=0.4 --dpi=96 /usr/share/code/code "--unity-launch %F" &'
