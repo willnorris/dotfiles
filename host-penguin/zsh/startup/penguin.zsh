30c14f52ea24284c60fcdaa2e0dec2ca0da979c9
Will Norris <will@willnorris.com>
2019-05-18 17:03:32 -0700

    zsh: add alias for chromium in crostini

--- host-penguin/zsh/startup/penguin.zsh
+++ host-penguin/zsh/startup/penguin.zsh
@@ -6,6 +6,7 @@ if [[ "$TERM" == "xterm" ]]; then
   export TERM="xterm-256color"
 fi
 
+alias chromium='sommelier -X --scale=0.5 --dpi=96 /usr/bin/chromium &'
+alias code='sommelier -X --scale=0.4 --dpi=96 /usr/share/code/code "--unity-launch %F" &'
 alias gimp='sommelier -X --scale=0.4 --dpi=96 /usr/bin/gimp &'
 alias inkscape='sommelier -X --scale=0.4 --dpi=96 /usr/bin/inkscape &'
-alias code='sommelier -X --scale=0.4 --dpi=96 /usr/share/code/code "--unity-launch %F" &'
