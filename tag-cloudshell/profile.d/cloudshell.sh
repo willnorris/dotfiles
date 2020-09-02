8641e03bd6770258c058767528d47f312108e5ab
Will Norris <will@willnorris.com>
2020-09-02 05:05:54 +0000

    add custom config for Google Cloud Shell
    the shell config doesn't actually work in zsh

---
+++ tag-cloudshell/profile.d/cloudshell.sh
@@ -0,0 +1,8 @@
+# If not running interactively, return
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+if [ -f "/google/devshell/bashrc.google" ]; then
+  source "/google/devshell/bashrc.google"
+fi
