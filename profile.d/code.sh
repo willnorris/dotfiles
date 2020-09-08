3e02c175bc63254264f3374512f1457723a9bb6f
Will Norris <will@willnorris.com>
2020-09-07 17:28:29 -0700

    sh: alias to prefer code-insiders

---
+++ profile.d/code.sh
@@ -0,0 +1,10 @@
+# Prefer VS Code Insiders.  Alias 'code' to 'code-insiders' if we're running in
+# a normal terminal or inside of VS Code Insiders.  The only time we don't
+# setup this alias is if we're running inside of VS Code stable.  That way, if
+# we're already in VS Code, 'code' will always point to the current version.
+if [ "${TERM_PROGRAM}" != "vscode" ] || [[ "${TERM_PROGRAM_VERSION}" == *"-insider" ]]; then
+  if command -v code-insiders >/dev/null; then
+    alias code=code-insiders
+  fi
+fi
+
