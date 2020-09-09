66b2d51a47cf3bcf26c3b0c0d27d726dcda96463
Will Norris <will@willnorris.com>
2020-09-09 11:12:21 -0700

    node: replace nvm with nodenv

--- profile.d/node.sh
+++ profile.d/node.sh
@@ -1,3 +1,17 @@
 if [ -d /opt/node ]; then
   PATH="/opt/node/bin:$PATH"
 fi
+
+[ -z "$PS1" ] && return # interactive only
+
+if [ -z "$NODENV_ROOT" ] && [ -d $HOME/.nodenv ]; then
+  export NODENV_ROOT=$HOME/.nodenv
+fi
+
+if [ -n "$NODENV_ROOT" ]; then
+  PATH="$NODENV_ROOT/bin:$PATH"
+fi
+
+if command -v nodenv >/dev/null; then
+  eval "$(nodenv init -)"
+fi
