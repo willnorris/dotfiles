7922003d643ec733bd25b4b3a2c8e5f29b9b39b7
Will Norris <will@willnorris.com>
2020-12-10 16:51:34 -0800

    zsh: remove homebrew config (again)

--- profile.d/0_homebrew.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$HOMEBREW_HOME" ]; then
-  if [ -d /usr/local/homebrew ]; then
-    export HOMEBREW_HOME=/usr/local/homebrew
-  fi
-fi
-
-if [ -n "$HOMEBREW_HOME" ]; then
-  PATH="$HOMEBREW_HOME/bin:$PATH"
-fi
