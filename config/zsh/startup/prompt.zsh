42cc84393115d6edde71ab278e557fbbe3134d4e
Will Norris <will@willnorris.com>
2021-03-18 10:30:47 -0700

    zsh/pure: cleanup responsive code
    this isn't necessarily shorter, but feels a little less hacky.  This
    also fixes refreshing the prompt on window resizing, which apparently
    can't be done inside the prompt setup file.

--- config/zsh/startup/prompt.zsh
+++ config/zsh/startup/prompt.zsh
@@ -18,3 +18,8 @@ export PURE_PROMPT_SYMBOL="%#"
 export PURE_PROMPT_VICMD_SYMBOL=":"
 zstyle :prompt:pure:prompt:success color $prompt_color
 zstyle :prompt:pure:path color $prompt_color
+
+# re-render preprompt when window changes
+TRAPWINCH() {
+  prompt_pure_preprompt_render
+}
