ae0fe59af7f1a8b026f83accd2322cb65f55a17b
Will Norris <will@willnorris.com>
2021-03-18 15:15:35 -0700

    vim: better handling of XDG variables

--- config/shell/_xdg_base.sh
+++ config/shell/_xdg_base.sh
@@ -1,4 +1,6 @@
 # variables for default XDG user directories
+export XDG_CONFIG_HOME XDG_CACHE_HOME XDG_DATA_HOME
+
 : ${XDG_CONFIG_HOME:="${HOME}/.config"}
 : ${XDG_CACHE_HOME:="${HOME}/.cache"}
 : ${XDG_DATA_HOME:="${HOME}/.local/share"}
