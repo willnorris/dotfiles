b58f140e36aec5692a59668d05e9b14f93098630
Will Norris <will@willnorris.com>
2021-03-17 22:29:26 -0700

    zsh: set pure prompt color based on hostname

--- config/zsh/startup/prompt.zsh
+++ config/zsh/startup/prompt.zsh
@@ -6,5 +6,14 @@ autoload -Uz promptinit
 promptinit
 prompt pure
 
-export PURE_PROMPT_SYMBOL="%%"
-zstyle :prompt:pure:prompt:success color blue
+local color=$(case $HOSTNAME in
+  levi)   echo green;;
+  rpi)    echo red;;
+  esther) echo cyan;;
+  vashti) echo cyan;;
+  *)      echo blue;;
+esac)
+
+export PURE_PROMPT_SYMBOL="%#"
+zstyle :prompt:pure:prompt:success color $color
+zstyle :prompt:pure:path color $color
