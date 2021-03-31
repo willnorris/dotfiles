0027f1c1652a6a9fa9cc853a4580b5d3e1ba48dd
Will Norris <will@willnorris.com>
2021-03-31 12:38:00 -0700

    shell: update go configuration
    I don't install go in /opt/go anymore, and there's no need to add
    GOROOT/bin to PATH explicitly

--- config/shell/go.sh
+++ config/shell/go.sh
@@ -1,11 +1,3 @@
-if [ -z "$GOROOT" ] && [ -d /opt/go ]; then
-  export GOROOT=/opt/go
-fi
-
-if [ -n "$GOROOT" ]; then
-  PATH="$GOROOT/bin:$PATH"
-fi
-
 if command -v go >/dev/null; then
   if [ -d "$(go env GOPATH)/bin" ]; then
     PATH="$(go env GOPATH)/bin:$PATH"
