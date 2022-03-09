fd1afd54536f78f0230a34d1915e3c163c4853a9
Will Norris <will@willnorris.com>
2022-03-08 17:45:21 -0800

    set default XDG_RUNTIME_DIR

--- config/shell/_xdg_base.sh
+++ config/shell/_xdg_base.sh
@@ -1,6 +1,7 @@
 # variables for default XDG user directories
-export XDG_CONFIG_HOME XDG_CACHE_HOME XDG_DATA_HOME
+export XDG_CONFIG_HOME XDG_CACHE_HOME XDG_DATA_HOME XDG_RUNTIME_DIR
 
 : "${XDG_CONFIG_HOME:="${HOME}/.config"}"
 : "${XDG_CACHE_HOME:="${HOME}/.cache"}"
 : "${XDG_DATA_HOME:="${HOME}/.local/share"}"
+: "${XDG_RUNTIME_DIR:="${HOME}/.local/run"}"
