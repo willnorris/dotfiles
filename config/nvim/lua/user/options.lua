f4810df1b6187ae3cb2eeed37378fe7e866415a3
Will Norris <will@willnorris.com>
2022-05-21 18:37:55 -0700

    nvim: reload cursor position on file open

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -55,3 +55,10 @@ vim.g.netrw_browse_split = 4
 vim.g.netrw_altv = 1
 vim.g.netrw_winsize = -28
 
+-- jump to last cursor position
+vim.cmd [[
+    autocmd BufReadPost *
+      \ if line("'\"") >= 1 && line("'\"") <= line("$") |
+      \   exe "normal! g`\"" |
+      \ endif
+]]
