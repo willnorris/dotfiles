09692a52b9b012a9c45989b913ce483ac7b1f52e
Will Norris <will@willnorris.com>
2022-06-12 11:20:53 -0700

    keychain: switch to ed25519 keys

--- config/shell/keychain.sh
+++ config/shell/keychain.sh
@@ -1,8 +1,8 @@
 [ -z "$PS1" ] && return # interactive only
 
 if command -v keychain >/dev/null; then
-  if [ -f "$HOME/.ssh/id_rsa" ]; then
+  if [ -f "$HOME/.ssh/id_ed25519" ]; then
     mkdir -p "${XDG_RUNTIME_DIR}/keychain"
-    eval "$(keychain --dir "${XDG_RUNTIME_DIR}/keychain" --absolute --eval --quiet --agents ssh id_rsa)"
+    eval "$(keychain --dir "${XDG_RUNTIME_DIR}/keychain" --absolute --eval --quiet --agents ssh id_ed25519)"
   fi
 fi
