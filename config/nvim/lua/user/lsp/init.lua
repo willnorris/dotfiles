9b29e8d730c1d80d38faad8ce189ed76322e1e35
Will Norris <will@willnorris.com>
2022-06-12 21:58:49 -0700

    nvim: fix border on lspinfo window

--- config/nvim/lua/user/lsp/init.lua
+++ config/nvim/lua/user/lsp/init.lua
@@ -5,3 +5,12 @@ end
 
 require("user.lsp.lsp-installer")
 require("user.lsp.handlers").setup()
+
+-- Fix LspInfo window border (https://neovim.discourse.group/t/1566)
+local win = require('lspconfig.ui.windows')
+local _default_opts = win.default_opts
+win.default_opts = function(options)
+  local opts = _default_opts(options)
+  opts.border = 'rounded'
+  return opts
+end
