61ad7e6c27b1a12c0239dd000f60c3c2e165c930
Will Norris <will@tailscale.com>
2022-07-06 01:14:08 -0500

    nvim: add lsp config for gopls

---
+++ config/nvim/lua/user/lsp/settings/gopls.lua
@@ -0,0 +1,17 @@
+local util = require "lspconfig/util"
+
+local opt = {
+  cmd = {"gopls", "serve"},
+  filetypes = {"go", "gomod"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
+
+return opt
