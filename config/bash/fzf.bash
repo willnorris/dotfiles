a66bfca98e03ada2304ee4b0cce40f9c651d44c7
Will Norris <will@tailscale.com>
2022-07-15 12:54:32 -0400

    bash,zsh: use additional location for fzf files

--- config/bash/fzf.bash
+++ config/bash/fzf.bash
@@ -1,10 +1,14 @@
 if command -v fzf > /dev/null; then
   if [ -f "$FZF_HOME/shell/completion.bash" ]; then
     source "$FZF_HOME/shell/completion.bash"
+  elif [ -f "/usr/share/doc/fzf/examples/completion.bash" ]; then
+    source "/usr/share/doc/fzf/examples/completion.bash"
   fi
 
   set -o vi
   if [ -f "$FZF_HOME/shell/key-bindings.bash" ]; then
     source "$FZF_HOME/shell/key-bindings.bash"
+  elif [ -f "/usr/share/doc/fzf/examples/key-bindings.bash" ]; then
+    source "/usr/share/doc/fzf/examples/key-bindings.bash"
   fi
 fi
--- config/zsh/startup/fzf.zsh
+++ config/zsh/startup/fzf.zsh
@@ -1,10 +1,14 @@
 if (( $+commands[fzf] )); then
   if [ -f "$FZF_HOME/shell/completion.zsh" ]; then
     source "$FZF_HOME/shell/completion.zsh"
+  elif [ -f "/usr/share/doc/fzf/examples/completion.zsh" ]; then
+    source "/usr/share/doc/fzf/examples/completion.zsh"
   fi
 
   bindkey -v
   if [ -f "$FZF_HOME/shell/key-bindings.zsh" ]; then
     source "$FZF_HOME/shell/key-bindings.zsh"
+  elif [ -f "/usr/share/doc/fzf/examples/key-bindings.zsh" ]; then
+    source "/usr/share/doc/fzf/examples/key-bindings.zsh"
   fi
 fi
