35f2c1d60c5c963363562365462102ba912af1e0
Will Norris <will@tailscale.com>
2022-07-16 10:54:25 -0700

    nvim: better keymaps for gitsigns

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -73,7 +73,27 @@ return packer.startup(function(use)
         signs = {
           add = { text = "+" },
           change = { text = "~" },
-        }
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          vim.keymap.set('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true, buffer=bufnr})
+
+          vim.keymap.set('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true, buffer=bufnr})
+
+          vim.keymap.set("n", "yogs", gs.toggle_signs)
+          vim.keymap.set("n", "<leader>gs", gs.stage_hunk)
+          vim.keymap.set("n", "<leader>gu", gs.undo_stage_hunk)
+          vim.keymap.set("n", "<leader>gr", gs.reset_hunk)
+          vim.keymap.set("n", "<leader>gp", gs.preview_hunk)
+        end,
       }
       vim.keymap.set("n", "yogs", "<Cmd>Gitsigns toggle_signs<CR>")
       vim.keymap.set("n", "]c", require("gitsigns").next_hunk)
