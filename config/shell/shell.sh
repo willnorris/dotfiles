c13a39467a2ffc2f8607b540629cbc779908a3d0
Will Norris <will@willnorris.com>
2022-07-16 17:56:52 -0700

    shell: remove ack alias
    I haven't used the original ack in many years, but have continued to use
    the alias. I supposed it's time to retrain my muscle memory to use 'rg'
    directly.

--- config/shell/shell.sh
+++ config/shell/shell.sh
@@ -1,12 +1,5 @@
 [ -z "$PS1" ] && return # interactive only
 
-if command -v rg >/dev/null; then
-  alias rg='rg -S'
-  alias ack='rg'
-elif command -v ag >/dev/null; then
-  alias ack='ag'
-fi
-
 if command -v batcat >/dev/null; then
   alias bat='batcat'
 fi
@@ -15,7 +8,6 @@ if command -v bat >/dev/null; then
   export BAT_STYLE="plain"
   alias cat='bat'
 fi
-
 
 alias vi=vim
 alias view='vim -R'
@@ -32,7 +24,7 @@ export VISUAL=${EDITOR}
 export PAGER='less'
 export LYNX_LSS=${HOME}/.lynx.lss
 
-if echo "a" | grep --color "a" >/dev/null 2>/dev/null; then
+if echo "a" | grep --color "a" >/dev/null 2>&1; then
   alias grep='grep --color=auto'
   alias fgrep='fgrep --color=auto'
   alias egrep='egrep --color=auto'
