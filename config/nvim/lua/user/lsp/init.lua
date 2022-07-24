c898c9b5a1f384aee9c467445d44a6f20736e41c
Will Norris <will@willnorris.com>
2022-07-24 01:35:19 -0700

    nvim: only show lsp virtual text on warnings

--- config/nvim/lua/user/lsp/init.lua
+++ config/nvim/lua/user/lsp/init.lua
@@ -34,6 +34,13 @@ end
 -- setup handlers
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+vim.lsp.diagnostic.on_publish_diagnostics, {
+  -- Enable virtual text only on Warning or above
+  virtual_text = {
+    severity = { min = vim.diagnostic.severity.WARN },
+  },
+})
 
 -- use telescope LSP handlers
 local ok, tb = pcall(require, "telescope.builtin")
