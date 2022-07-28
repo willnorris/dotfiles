9f0e530792bc15a69445dcd1059d213c7c453ee6
Will Norris <will@willnorris.com>
2022-07-28 10:51:56 -0700

    shell: use secretive for ssh keys if available

--- config/shell/keychain.sh
+++ config/shell/keychain.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
 [ -z "$PS1" ] && return # interactive only
 
+# prefer secretive if installed (https://github.com/maxgoedjen/secretive)
+if [ -r "${HOME}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh" ]; then
+  export SSH_AUTH_SOCK="${HOME}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh"
+  return
+fi
+
+# then try keychain (https://www.funtoo.org/Keychain)
 if command -v keychain >/dev/null; then
   if [ -f "$HOME/.ssh/id_ed25519" ]; then
     mkdir -p "${XDG_RUNTIME_DIR}/keychain"
