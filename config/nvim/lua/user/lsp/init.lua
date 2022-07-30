fd6ac47700b7165af3e9a1a199fde29940046d69
Will Norris <will@willnorris.com>
2022-07-30 09:36:47 -0700

    nvim: update lsp-installer icons

--- config/nvim/lua/user/lsp/init.lua
+++ config/nvim/lua/user/lsp/init.lua
@@ -5,9 +5,9 @@ lsp_installer.setup({
   ui = {
     border = "rounded",
     icons = {
-      server_installed = "✓",
-      server_pending = "➜",
-      server_uninstalled = "✗"
+      server_installed = "",
+      server_pending = "",
+      server_uninstalled = ""
     }
   },
 })
