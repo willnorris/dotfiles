d9ded8305f5de8a568e09f6338e9b2cf47cb895e
Will Norris <will@willnorris.com>
2023-01-12 14:30:17 -0800

    nvim: increase treesitter highlight file size limit to 100k
    treesitter seems to be handling 10,000 LOC files fine now.  Leave this
    disable logic in place (in case we need it later), but increase the
    limit to 100,000.

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -136,7 +136,7 @@ packer.startup({ function(use)
           disable = function(_, bufnr)
             -- treesitter highlighting seems to have trouble with large files. Partially disussed
             -- (but not really resolved) at https://github.com/nvim-treesitter/nvim-treesitter/issues/556
-            return vim.api.nvim_buf_line_count(bufnr) > 10000
+            return vim.api.nvim_buf_line_count(bufnr) > 100000
           end,
         },
         textobjects = {
