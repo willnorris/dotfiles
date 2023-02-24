7028c8c399b149fff2ef502d429bec6c674914e8
Will Norris <will@willnorris.com>
2023-02-23 23:56:29 -0800

    nvim: impatient is no longer needed

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,1 @@
-local ok, impatient = pcall(require, 'impatient')
-if ok then
-  impatient.enable_profile()
-else
-  vim.notify(impatient)
-end
-
--- Do all init in user/init.lua so impatient can cache it
 require("user")
--- config/nvim/lua/user/plugins/init.lua
+++ config/nvim/lua/user/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
 
-  "lewis6991/impatient.nvim", -- Caching for faster startup time
   "famiu/bufdelete.nvim", -- Better buffer deletion
   "farmergreg/vim-lastplace", -- Restore last cursor position
   "konfekt/vim-sentence-chopper", -- Semantic Line Breaks
