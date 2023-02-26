87bb9e2936e0b6afc7e47b9819177066dff6a435
Will Norris <will@willnorris.com>
2023-02-26 13:46:31 -0800

    nvim: refactor treesitter config

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -15,7 +15,7 @@ return {
   "nvim-tree/nvim-web-devicons",
   "isobit/vim-caddyfile", -- Caddyfile syntax support
 
-  { "echasnovski/mini.pairs",              enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
 
   -- Copy text to clipboard with OSC52
   {
@@ -83,15 +83,28 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-      pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-    end,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
+    opts = {
+      ensure_installed = { "go" },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        disable = function(_, bufnr)
+          -- treesitter highlighting seems to have trouble with large files. Partially disussed
+          -- (but not really resolved) at https://github.com/nvim-treesitter/nvim-treesitter/issues/556
+          return vim.api.nvim_buf_line_count(bufnr) > 100000
+        end,
+      },
+      incremental_selection = {
+        enable = true,
+        -- I don't love these default keymaps, but <c-space> from LazyVim conflicts with my tmux config
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
     },
-    config = function()
-      require("config.treesitter")
-    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
