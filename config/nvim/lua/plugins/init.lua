b2d99bb2787ac8da20e27e499eda979ede1c0baf
Will Norris <will@willnorris.com>
2023-02-27 10:13:51 -0800

    nvim: fix treesitter opts

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -83,28 +83,34 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = { "go" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = function(_, bufnr)
-          -- treesitter highlighting seems to have trouble with large files. Partially disussed
-          -- (but not really resolved) at https://github.com/nvim-treesitter/nvim-treesitter/issues/556
-          return vim.api.nvim_buf_line_count(bufnr) > 100000
-        end,
-      },
-      incremental_selection = {
-        enable = true,
-        -- I don't love these default keymaps, but <c-space> from LazyVim conflicts with my tmux config
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter-context", opts = { mode = "topline" } },
     },
+    opts = function(_, opts)
+      opts.ensure_installed[#opts.ensure_installed + 1] = "go"
+
+      return vim.tbl_deep_extend("keep", {
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = function(_, bufnr)
+            -- treesitter highlighting seems to have trouble with large files. Partially disussed
+            -- (but not really resolved) at https://github.com/nvim-treesitter/nvim-treesitter/issues/556
+            return vim.api.nvim_buf_line_count(bufnr) > 100000
+          end,
+        },
+        incremental_selection = {
+          enable = true,
+          -- I don't love these default keymaps, but <c-space> from LazyVim conflicts with my tmux config
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      }, opts)
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
