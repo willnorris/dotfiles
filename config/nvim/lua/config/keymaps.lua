8e1b5671a33a2de1736c8428f8ae0d05accb6c74
Will Norris <will@willnorris.com>
2023-03-06 21:47:13 -0800

    nvim: restore default behavior for 'gw'
    unset keymap from LazyVim, and use a modified version of the workaround
    for null-ls to reset vim.bo.formatexpr when there is no formatting
    generator for the current buffer.
    
    Original comment: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131#issuecomment-1273843531

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -65,3 +65,15 @@ keymap("n", "<C-e>",
   end,
   { noremap = true, desc = "Treesitter group under cursor" }
 )
+
+-- Restore 'gw' to default behavior. First, remove the 'gw' keymap set in LazyVim:
+vim.keymap.del({ "n", "x" }, "gw")
+-- Then, reset formatexpr if null-ls is not providing any formatting generators.
+-- See: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131
+require("lazyvim.util").on_attach(function(client, buf)
+  if client.name == "null-ls" then
+    if not require("null-ls.generators").can_run(vim.bo[buf].filetype, require("null-ls.methods").lsp.FORMATTING) then
+      vim.bo[buf].formatexpr = nil
+    end
+  end
+end)
