115cd4c1f49d3d601cc2d1b3b788bc691c03e42f
Will Norris <will@willnorris.com>
2023-03-17 16:43:23 -0700

    nvim: customize LSP goto-preview plugin

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -134,7 +134,12 @@ return {
     "rmagatti/goto-preview",
     event = "VeryLazy",
     opts = {
+      border = {"┌", "─" ,"┐", "│", "┘", "─", "└", "│"};
       default_mappings = true,
+      resizing_mappings = true,
+      post_open_hook = function(_, winid)
+        vim.keymap.set("n", "q", function() vim.api.nvim_win_close(winid, false) end)
+      end
     },
   },
   {
