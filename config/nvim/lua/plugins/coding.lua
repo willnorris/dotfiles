37fa47e62bbcd6e9e77e71b051d46c3c2f615f4f
Will Norris <will@willnorris.com>
2023-03-24 20:24:28 -0700

    nvim: update keys and config for dap

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -120,8 +120,10 @@ return {
         { "<leader>dj", function() require("dap").step_over() end, desc = "debug: step over" },
         { "<leader>dl", function() require("dap").step_into() end, desc = "debug: step into" },
         { "<leader>dh", function() require("dap").step_out() end, desc = "debug: step out" },
-        { "<leader>dl", function() require("dap").run_last() end, desc = "debug: run last" },
+        { "<leader>da", function() require("dap").run_last() end, desc = "debug: run last" },
+        { "<leader>dr", function() require("dap").restart() end, desc = "debug: restart session" },
         { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "debug: toggle breakpoint" },
+        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "debug: toggle conditional breakpoint" },
         { "<leader>dt", function() require("dap").terminate() end, desc = "debug: terminate session" },
       },
       dependencies = {
@@ -133,7 +135,13 @@ return {
             commented = true,
           },
         },
-      }
+      },
+      config = function()
+        local dapui, dap = require("dapui"), require("dap")
+        dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+        dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+        dap.listeners.before.event_exited["dapui_config"] = dapui.close
+      end,
     },
   },
 
