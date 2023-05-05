802ad03db9782e93da58a87a9573d6e1529c7e6d
Will Norris <will@willnorris.com>
2023-05-05 12:18:48 -0700

    zsh: fix path ordering on macOS
    There is a long history of macOS messing up ordering of elements in
    $PATH due to the path_helper utility, which is pretty well summarized at
    https://gist.github.com/Linerre/f11ad4a6a934dcf01ee8415c9457e7b2. For
    zsh, path_helper is called by the system /etc/zprofile, which is loaded
    after zshenv and before zshrc.
    
    If the value of $PATH changes between loading zshenv and zshrc, we
    source the local zshenv again so that any PATH elements can be nudged to
    the front again.  Note that this is after calling `typset -U path` to
    ensure entries aren't duplicated.
    
    This is not the first time trying to sort this out, and very well may
    not be the last, but it seems to be working pretty well for now.

--- config/shell/asdf.sh
+++ config/shell/asdf.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-if [ "${ASDF_LOADED}" = "1" ]; then
-  return
-fi
-
-export ASDF_LOADED=1
-
 export ASDF_DIR="${XDG_DATA_HOME}/asdf"
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
 export ASDF_DATA_DIR="${ASDF_DIR}"
@@ -14,6 +8,10 @@ export ASDF_PYTHON_DEFAULT_PACKAGES_FILE="${XDG_CONFIG_HOME}/pip/default-python-
 export ASDF_NPM_DEFAULT_PACKAGES_FILE="${XDG_CONFIG_HOME}/npm/default-npm-packages"
 export ASDF_GEM_DEFAULT_PACKAGES_FILE="${XDG_CONFIG_HOME}/gem/default-gems"
 
-if [ -f "${ASDF_DIR}/asdf.sh" ]; then
+# load asdf.sh as long as we're not in a subshell ($SHLVL > 1),
+# in which case it should have already been loaded.
+if [ ${SHLVL} -le 1 ] && [ -f "${ASDF_DIR}/asdf.sh" ]; then
   . "${ASDF_DIR}/asdf.sh"
+else
+  . "${ASDF_DIR}/lib/asdf.sh"
 fi
