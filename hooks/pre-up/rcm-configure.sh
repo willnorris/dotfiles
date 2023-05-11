acc8f95fbc3b68310391c4d503c531ca64702c07
Will Norris <will@willnorris.com>
2023-05-11 07:50:43 -0700

    rcm: simplify rcm pre-up hook
    move rcrc.example into rcm config directory and simplify copy command
    since rcm hooks always run in the local hook directory.

--- hooks/pre-up/rcm-configure.sh
+++ hooks/pre-up/rcm-configure.sh
@@ -4,14 +4,9 @@ set -eu
 
 . "../../config/shell/_base.sh"
 
-pushd "$(dirname "$0")" > /dev/null
-cd ../..
-dotfiles_dir="$(pwd)"
-popd > /dev/null
-
 : "${RCRC:="${XDG_CONFIG_HOME}/rcm/rcrc"}"
 if [ ! -f "${RCRC}" ]; then
   echo "INFO: Installing default ${RCRC} file" 1>&2
   mkdir -p "$(dirname "${RCRC}")"
-  cp "${dotfiles_dir}/rcrc.example" "${RCRC}"
+  cp "../../config/rcm/rcrc.example" "${RCRC}"
 fi
