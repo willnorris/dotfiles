9ee3256ecd2b8f3d70d7a0fd6c083c45e921a3f7
Will Norris <will@willnorris.com>
2023-05-20 13:58:22 -0700

    vendor: run update.sh scripts from anywhere

--- vendor/terminal-colors/update.sh
+++ vendor/terminal-colors/update.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+pushd "$(dirname "$0")" || exit
 
 curl -OL "https://raw.githubusercontent.com/eikenb/terminal-colors/master/terminal-colors"
+
+popd || exit
--- vendor/z/update.sh
+++ vendor/z/update.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+pushd "$(dirname "$0")" || exit
 
 curl -OL "https://raw.githubusercontent.com/rupa/z/master/z.sh"
 curl -OL "https://raw.githubusercontent.com/rupa/z/master/z.1"
+
+popd || exit
