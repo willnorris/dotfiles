28a63ea8a7588e7f470758792c71a8040bedc734
Will Norris <will@willnorris.com>
2023-06-02 22:35:38 -0700

    nvim: use nvim-dap from LazyVim

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -112,36 +112,27 @@ return {
   },
 
   {
+    { import = "lazyvim.plugins.extras.dap.core" },
     {
-      "mfussenegger/nvim-dap",
-      keys = {
-        { "<leader>dd", function() require("dapui").toggle() end, desc = "debug: toggle UI" },
-        { "<leader>dk", function() require("dap").continue() end, desc = "debug: continue" },
-        { "<leader>dj", function() require("dap").step_over() end, desc = "debug: step over" },
-        { "<leader>dl", function() require("dap").step_into() end, desc = "debug: step into" },
-        { "<leader>dh", function() require("dap").step_out() end, desc = "debug: step out" },
-        { "<leader>da", function() require("dap").run_last() end, desc = "debug: run last" },
-        { "<leader>dr", function() require("dap").restart() end, desc = "debug: restart session" },
-        { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "debug: toggle breakpoint" },
-        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "debug: toggle conditional breakpoint" },
-        { "<leader>dt", function() require("dap").terminate() end, desc = "debug: terminate session" },
-      },
-      dependencies = {
-        { "rcarriga/nvim-dap-ui", config = true },
-        { "leoluz/nvim-dap-go", config = true },
-        {
-          "theHamsta/nvim-dap-virtual-text",
-          opts = {
-            commented = true,
-          },
+      "jay-babu/mason-nvim-dap.nvim",
+      opts = {
+        handlers = {
+          delve = function(config)
+            config.adapters.executable.args = vim.list_extend(config.adapters.executable.args,
+              { "--check-go-version=false" })
+            config.configurations = vim.list_extend(config.configurations or {}, {
+              {
+                type = "delve",
+                name = "Delve: Attach",
+                mode = "local",
+                request = "attach",
+                processId = require("dap.utils").pick_process,
+              },
+            })
+            require("mason-nvim-dap").default_setup(config)
+          end,
         },
       },
-      config = function()
-        local dapui, dap = require("dapui"), require("dap")
-        dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-        dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-        dap.listeners.before.event_exited["dapui_config"] = dapui.close
-      end,
     },
   },
 
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -103,6 +103,7 @@ return {
     },
   },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   {
