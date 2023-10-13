8c701bc8b95f6824df0c22ff5f627bbe44a231cb
Will Norris <will@willnorris.com>
2023-10-13 13:58:57 -0700

    nvim: disable stylua in conform

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -119,6 +119,15 @@ return {
       },
     },
   },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft.lua = nil
+      return opts
+    end,
+  },
+
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
   {
     "nvimtools/none-ls.nvim",
