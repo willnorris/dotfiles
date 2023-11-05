3d0816b2d6bd897766c6a82dd0d2944d3acec06f
Will Norris <will@willnorris.com>
2023-11-04 20:16:33 -0700

    hooks: add hook to download coding font

---
+++ hooks/post-up/fonts.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Download and install Cascadia Code Nerd Font Mono
+
+if [ "$(uname)" = "Linux" ]; then
+  FONTS="${HOME}/.local/share/fonts"
+elif [ "$(uname)" = "Darwin" ]; then
+  FONTS="${HOME}/Library/Fonts"
+fi
+
+FILE="CaskaydiaCoveNerdFontMono-Regular.ttf"
+if [ ! -f "${FONTS}/${FILE}" ]; then
+  mkdir -p "${FONTS}"
+  TMP=$(mktemp -d)
+  pushd "${TMP}" || exit
+  curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.tar.xz"
+  tar -xJf CascadiaCode.tar.xz
+  mv "${FILE}" "${FONTS}"
+  popd || exit
+  rm -rf "${TMP}"
+fi
