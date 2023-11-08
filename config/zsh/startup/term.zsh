621d390be874f3639c720d2c22af572e20872b3f
Will Norris <will@willnorris.com>
2023-11-07 22:03:55 -0800

    zsh: set colors in Linux virtual console
    This is the console displayed on boot in multi-user mode.
    
    From https://wiki.archlinux.org/title/Color_output_in_console#Virtual_console

---
+++ config/zsh/startup/term.zsh
@@ -0,0 +1,6 @@
+# set colors in Linux virtual console.
+
+if [ "${TERM}" = "linux" ]; then
+  echo "\e]P01e1f24\e]P1e06c75\e]P298c379\e]P3e5c07b\e]P461afef\e]P5c678dd\e]P656b6c2\e]P7abb2bf"
+  echo "\e]P83e4452\e]P9e06c75\e]PA98c379\e]PBe5c07b\e]PC61afef\e]PDc678dd\e]PE56b6c2\e]PFabb2bf"
+fi
