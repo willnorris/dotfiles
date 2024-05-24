767ea8228b0970a2828f70cd5a581377622edd12
Will Norris <will@willnorris.com>
2024-05-23 18:34:46 -0700

    nvim: remove orgmode

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,12 +1,1 @@
-return {
-  {
-    "nvim-orgmode/orgmode",
-    opts = {
-      org_agenda_files = "~/.local/share/orgmode/*",
-      org_default_notes_file = "~/.local/share/orgmode/notes.org",
-    },
-    config = function(_, opts)
-      require("orgmode").setup(opts)
-    end,
-  },
-}
+return {}
