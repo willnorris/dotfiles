b0cd62106c0cd0f8eb0e04bc27349bda137f8262
Will Norris <will@willnorris.com>
2024-05-26 21:06:19 -0700

    shell: add adb alias

--- config/shell/android.sh
+++ config/shell/android.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # Location of Android Studio files on macOS
-if [ -d "${HOME}/Library/Android/sdk/" ]; then
-  export ANDROID_SDK_ROOT="${HOME}/Library/Android/sdk/"
+if [ -d "${HOME}/Library/Android/sdk" ]; then
+  export ANDROID_SDK_ROOT="${HOME}/Library/Android/sdk"
+fi
+
+if [ -x "${ANDROID_SDK_ROOT}/platform-tools/adb" ]; then
+  alias adb="${ANDROID_SDK_ROOT}/platform-tools/adb"
 fi
