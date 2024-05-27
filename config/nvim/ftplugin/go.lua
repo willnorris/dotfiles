7708f7b57b700182126b3cd770abffe05853a326
Will Norris <will@willnorris.com>
2024-05-27 09:07:16 -0700

    nvim: remove unnecessary go format autocmd

--- config/nvim/ftplugin/go.lua
+++ config/nvim/ftplugin/go.lua
@@ -6,14 +6,3 @@ vim.opt_local.spell = true
 
 -- Go has no max line-length, but set colorcolumn as a reference
 vim.opt_local.colorcolumn = "81"
-
-local augroup = vim.api.nvim_create_augroup("goformat", { clear = true })
-
--- format and organize imports on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup,
-  pattern = "*.go",
-  callback = function()
-    vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-  end,
-})
