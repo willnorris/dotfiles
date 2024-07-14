37b50ffd5533a8459919b1a85ea6102cd8b8f360
Will Norris <will@willnorris.com>
2024-07-14 10:38:53 -0700

    zsh: look for fzf scrippts in /usr/share/fzf
    This is the location they get installed to in Arch Linux, and possibly
    other distributions.

--- config/zsh/startup/fzf.zsh
+++ config/zsh/startup/fzf.zsh
@@ -1,6 +1,8 @@
 if (( $+commands[fzf] )); then
   if [ -f "$FZF_HOME/shell/completion.zsh" ]; then
     source "$FZF_HOME/shell/completion.zsh"
+  elif [ -f "/usr/share/fzf/completion.zsh" ]; then
+    source "/usr/share/fzf/completion.zsh"
   elif [ -f "/usr/share/doc/fzf/examples/completion.zsh" ]; then
     source "/usr/share/doc/fzf/examples/completion.zsh"
   fi
@@ -8,6 +10,8 @@ if (( $+commands[fzf] )); then
   bindkey -v
   if [ -f "$FZF_HOME/shell/key-bindings.zsh" ]; then
     source "$FZF_HOME/shell/key-bindings.zsh"
+  elif [ -f "/usr/share/fzf/key-bindings.zsh" ]; then
+    source "/usr/share/fzf/key-bindings.zsh"
   elif [ -f "/usr/share/doc/fzf/examples/key-bindings.zsh" ]; then
     source "/usr/share/doc/fzf/examples/key-bindings.zsh"
   fi
