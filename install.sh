ee29081fb1a8c1bf6cef50e44e981a5290382ae5
Will Norris <will@willnorris.com>
2024-08-19 12:34:58 -0700

    install.sh: allow install script to work on all hosts
    Originally this was just added for codespaces, since that will
    automatically call install.sh if it is present.  But there's not reason
    we can't just use this for all environments, which makes the initial
    install a tad eaiser (not having to manually setup PATH for the first
    rcup call).

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-# link installed dotfiles location to ~/.dotfiles
+# if dotfiles have been installed anywhere other than ~/.dotfiles,
+# add a link there. This is mainly for GitHub codespaces.
 DOTFILE_DIR="$(dirname "$(realpath "$0")")"
 if [ -d "${DOTFILE_DIR}" ] && [ ! -d "${HOME}/.dotfiles" ]; then
   ln -s "${DOTFILE_DIR}" "${HOME}/.dotfiles"
 fi
 
-# run the codespaces install script.
+# If we're running in codespaces, run the install script,
+# otherwise just run rcup.
 if [ "$CODESPACES" = "true" ]; then
   . "${HOME}/.dotfiles/tag-codespaces/install.sh"
+else
+  PATH="${HOME}/.dotfiles/local/bin:${PATH}" rcup
 fi
