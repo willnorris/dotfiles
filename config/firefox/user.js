8b64899cc57682cab8da308c445a8ca654a43e10
Will Norris <will@willnorris.com>
2024-08-19 13:29:51 -0700

    firefox: add user.js with custom settings
    This needs to be copied or symlinked into the firefox profile directory.
    Eventually I should figure out a way to link these automatically as part
    of an rcm hook or something.

---
+++ config/firefox/user.js
@@ -0,0 +1,15 @@
+// Disable about:config warning
+user_pref("browser.aboutConfig.showWarning ", false);
+
+// Disable menu when pressing alt key.
+user_pref("ui.key.menuAccessKeyFocuses ", false);
+
+// Allow go/links to work.
+// Repeat for any other bare hostnames to allow.
+user_pref("browser.fixup.domainwhitelist.go ", true);
+
+// Limit scroll distance when "fling" scrolling
+// There are additional apz.fling_* options,
+// but at a minimum these seem to work well.
+user_pref("apz.fling_friction", "0.007");
+user_pref("apz.fling_min_velocity_threshold", "1.5");
