b48d26a88892313b80e509b79c5bddf93e19bb29
Will Norris <will@willnorris.com>
2024-11-13 22:36:19 -0800

    nvim: remove most of custom lspconfig
    Most of this was for additional keymaps, some of which is unnecessary
    with updates to LazyVim and various plugins, and some of which I just
    never used anyway.
    
    Also remove lua_ls customizations, which seem to be handled by
    lazydev.nvim now.

--- config/nvim/lua/plugins/lang.lua
+++ config/nvim/lua/plugins/lang.lua
@@ -17,75 +17,8 @@ return {
 
   { -- lspconfig
     "neovim/nvim-lspconfig",
-    init = function()
-      -- setup handlers
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-      vim.lsp.handlers["textDocument/signatureHelp"] =
-          vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        -- Enable virtual text only on Warning or above
-        virtual_text = {
-          severity = { min = vim.diagnostic.severity.WARN },
-        },
-      })
-
-      -- use telescope LSP handlers
-      local tb = require("telescope.builtin")
-      vim.lsp.handlers["callHierarchy/incomingCalls"] = tb.lsp_incoming_calls
-      vim.lsp.handlers["callHierarchy/outgoingCalls"] = tb.lsp_outgoing_calls
-      vim.lsp.handlers["textDocument/references"] = tb.lsp_references
-      vim.lsp.handlers["textDocument/typeDefinition"] = tb.lsp_type_definitions
-      vim.lsp.handlers["textDocument/implementation"] = tb.lsp_implementations
-      vim.lsp.handlers["textDocument/documentSymbol"] = tb.lsp_document_symbols
-      vim.lsp.handlers["workspace/symbol"] = tb.lsp_workspace_symbols
-      -- causes issues with goto-preview
-      --vim.lsp.handlers["textDocument/definition"] = tb.lsp_definitions
-
-      -- override some keymaps from LazyVim
-      local keymaps = require("lazyvim.plugins.lsp.keymaps")
-      local keys = keymaps.get()
-      --keys[#keys + 1] = { "gd", vim.lsp.buf.definition, desc = "Goto Definition" }
-      keys[#keys + 1] = { "gn", vim.lsp.buf.incoming_calls, desc = "Incoming Calls" }
-      keys[#keys + 1] = { "gi", vim.lsp.buf.implementation, desc = "Goto Implementation" }
-      keys[#keys + 1] = { "gt", vim.lsp.buf.type_definition, desc = "Goto Type Definition" }
-      keys[#keys + 1] = { "gl", vim.lsp.codelens.run, desc = "Run codelens" }
-      keys[#keys + 1] = { "gk", vim.lsp.buf.signature_help, desc = "Signature Help" }
-
-      -- workspace folders
-      require("which-key").register({ ["<leader>w"] = { name = "+workspace" } })
-      keys[#keys + 1] = { "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "add workspace folder" }
-      keys[#keys + 1] = { "<leader>wr", vim.lsp.buf.remove_workspace_folder, desc = "remove workspace folder" }
-      keys[#keys + 1] = {
-        "<leader>wl",
-        function()
-          vim.notify(
-            vim.pretty_print(vim.lsp.buf.list_workspace_folders()),
-            vim.log.levels.INFO,
-            { title = "LSP Workspace folders" }
-          )
-        end,
-        desc = "list workspace folders",
-      }
-
-      require("lspconfig.ui.windows").default_options.border = "rounded"
-    end,
     opts = {
       servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-              workspace = {
-                library = {
-                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                  [vim.fn.stdpath("config") .. "/lua"] = true,
-                },
-              },
-            },
-          },
-        },
         gopls = {
           settings = {
             gopls = {
