99f059718efb02099f210deb111888b233a5b5fc
Will Norris <will@willnorris.com>
2024-12-18 21:33:25 -0800

    fzf: update keymaps and preview style
    This aligns more closely with what I was using in vim with telescope and
    now with fzf-lua.

--- config/shell/fzf.sh
+++ config/shell/fzf.sh
@@ -7,9 +7,6 @@ fi
 if [ -z "$FZF_HOME" ] && [ -d "$XDG_DATA_HOME/fzf" ]; then
   export FZF_HOME="${XDG_DATA_HOME}/fzf"
 fi
-if [ -z "$FZF_HOME" ] && [ -d "$XDG_DATA_HOME/nvim/site/pack/packer/start/fzf" ]; then
-  export FZF_HOME="${XDG_DATA_HOME}/nvim/site/pack/packer/start/fzf"
-fi
 
 if [ -n "$FZF_HOME" ]; then
   pathadd "$FZF_HOME/bin"
@@ -21,11 +18,15 @@ if has fzf; then
   fi
   export FZF_DEFAULT_OPTS="
   --color dark,bg+:8,gutter:-1,hl:1,hl+:1,info:2 --no-bold --height '40%' --reverse
-  --preview '(bat -p --color always {} || cat {} || tree -C -L 1 -F {}) 2> /dev/null | head -300'
+  --preview '(bat --style=numbers,changes --color=always {} || cat {} || tree -C -L 1 -F {}) 2> /dev/null | head -300'
   --preview-window 'right:70%'
-  --bind '?:toggle-preview,alt-j:preview-down,alt-k:preview-up'
-  --bind 'alt-space:preview-page-down,alt-f:preview-page-down,alt-b:preview-page-up'
+  --marker='+'
   --bind 'ctrl-f:page-down,ctrl-b:page-up'
+  --bind 'ctrl-d:half-page-down,ctrl-u:half-page-up'
+  --bind 'alt-j:preview-down,alt-k:preview-up'
+  --bind 'alt-f:preview-page-down,alt-b:preview-page-up'
+  --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
+  --bind 'ctrl-o:toggle-preview'
   "
   export FZF_CTRL_R_OPTS="--no-reverse"
   export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
