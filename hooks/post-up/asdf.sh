65442de97e2d4420b7098e6bb4be598109316408
Will Norris <will@willnorris.com>
2025-01-07 23:58:59 -0800

    hooks: remove surrounding "$()" for has_network function

--- hooks/post-up/asdf.sh
+++ hooks/post-up/asdf.sh
@@ -2,6 +2,6 @@
 
 . "${HOME}/.config/shell/_base.sh"
 
-if [ ! -d "${XDG_DATA_HOME}/asdf" ] && $(has_network); then
+if [ ! -d "${XDG_DATA_HOME}/asdf" ] && has_network; then
   git clone https://github.com/asdf-vm/asdf.git "${XDG_DATA_HOME}/asdf"
 fi
