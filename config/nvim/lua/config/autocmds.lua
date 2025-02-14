cf1f850e93b997aba959dc99e8909d1fb13ff275
Will Norris <will@willnorris.com>
2025-02-13 21:49:07 -0800

    nvim: disable focus plugin for snacks.picker

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd("WinEnter", {
 vim.api.nvim_create_autocmd("FileType", {
   group = focus_augroup,
   callback = function(_)
-    local ignore_filetypes = { "fugitive", "gitcommit", "outline", "neo-tree" }
+    local ignore_filetypes = { "fugitive", "gitcommit", "outline", "neo-tree", "snacks_picker_list" }
     if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
       vim.b.focus_disable = true
     end
