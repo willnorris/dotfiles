a918191c3d93b6520fba33484ccaceebed763987
Will Norris <will@willnorris.com>
2025-03-28 21:11:47 -0700

    linux: remove hyprland and related config

--- tag-linux-desktop/config/hypr/autolock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "${HOME}/.config/shell/_base.sh"
-
-if has swayidle; then
-	swayidle -w timeout 300 'swaylock -f' timeout 600 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
-else
-	echo "swayidle not installed"
-fi
