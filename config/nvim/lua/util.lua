cd2afc53c037fde266ada28d0a2edf1390a9dae9
Will Norris <will@willnorris.com>
2025-05-09 21:29:22 -0700

    nvim: apply tmux mod key mapping before OS-specific
    Technically this is a noop, since <M-*> and <A-*> have the same effect.
    But in principal, using tmux should take precedence, since it doesn't
    matter what operating system it is running in.

--- config/nvim/lua/util.lua
+++ config/nvim/lua/util.lua
@@ -4,15 +4,15 @@ M = {}
 
 -- mod returns a keybinding string with the appropriate modifier key and k.
 -- It uses the modifier key that is two keys to the left of the space bar.
+-- If tmux is active, in which case it uses <A-k>,
+-- because tmux doesn't suppport the Super key and sends Alt instead.
 -- On macOS, it uses <M-k> for the Option key.
 -- On all other platforms, it uses <D-k> for the Super key.
--- Unless tmux is active, in which case it uses <A-k>,
--- because tmux doesn't suppport the Super key and sends Alt instead.
 M.mod = function(k)
-  if vim.fn.has("mac") == 1 then
-    return "<M-" .. k .. ">"
-  elseif os.getenv("TMUX") ~= nil then
+  if os.getenv("TMUX") ~= nil then
     return "<A-" .. k .. ">"
+  elseif vim.fn.has("mac") == 1 then
+    return "<M-" .. k .. ">"
   else
     return "<D-" .. k .. ">"
   end
