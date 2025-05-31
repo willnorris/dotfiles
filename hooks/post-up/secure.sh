3ac5bca86b319bd62183a032068b9336a8f02fe1
Will Norris <will@willnorris.com>
2025-05-30 20:02:19 -0700

    rcm/hooks: format secure.sh script

--- hooks/post-up/secure.sh
+++ hooks/post-up/secure.sh
@@ -8,9 +8,10 @@ function link_secure() {
   link_file "${HOME}/.secure/${1}" "${HOME}/${2}"
 }
 
-link_secure "gpg/pubring.gpg"                   ".gnupg/pubring.gpg"
-link_secure "gpg/private-keys-v1.d"             ".gnupg/private-keys-v1.d"
-link_secure "mutt/credentials"                  ".config/neomutt/credentials"
-link_secure "mysql/${HOST}.cnf"                 ".my.cnf"
-link_secure "netrc/${HOST}.netrc"               ".netrc"
-link_secure "rclone/wjn730-b321c955b53f.json"   ".config/rclone/wjn730-b321c955b53f.json"
+link_secure "gpg/pubring.gpg" ".gnupg/pubring.gpg"
+link_secure "gpg/pubring.kbx" ".gnupg/pubring.kbx"
+link_secure "gpg/private-keys-v1.d" ".gnupg/private-keys-v1.d"
+link_secure "mutt/credentials" ".config/neomutt/credentials"
+link_secure "mysql/${HOST}.cnf" ".my.cnf"
+link_secure "netrc/${HOST}.netrc" ".netrc"
+link_secure "rclone/wjn730-b321c955b53f.json" ".config/rclone/wjn730-b321c955b53f.json"
