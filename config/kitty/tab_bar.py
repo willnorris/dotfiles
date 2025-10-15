02aed20fcbd4c03255b989e901921b03ff5f0cde
Will Norris <will@willnorris.com>
2025-10-15 10:15:57 -0700

    kitty: restructure config, custom tab bar, keyboard shortcuts
    This is my initial start at trying to replace tmux with native kitty
    functionality for tabs and windows. I'll likely still use tmux for
    persistent sessions, particularly on remote hosts, but I do that less
    and less these days.
    
    The tab bar config and keyboard shortcuts largely mirror what I have in
    tmux. My muscle memory for the ctrl+space is pretty strong, and at least
    right now I don't see a reason to change it, particularly since kitty
    has good support for multi-key mappings. This could cause problems when
    I run tmux on a remote host, but I still have a secondary ctrl+a prefix
    for tmux, so I'm not completely stuck.

---
+++ config/kitty/tab_bar.py
@@ -0,0 +1,61 @@
+from kitty.fast_data_types import get_options, Screen
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    Formatter,
+    TabBarData,
+    as_rgb,
+    draw_attributed_string,
+    draw_title,
+    get_boss,
+)
+from kitty.utils import color_as_int
+
+opts = get_options()
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_tab_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    # if we are in a named session, show it before tabs
+    if index == 1 and tab.session_name:
+        fg, bg = screen.cursor.fg, screen.cursor.bg
+        screen.cursor.bg = 0
+        screen.cursor.fg = as_rgb(color_as_int(opts.color4))
+        screen.draw(" " + tab.session_name + " ")
+        screen.cursor.fg, screen.cursor.bg = fg, bg
+
+    if draw_data.leading_spaces:
+        screen.draw(" " * draw_data.leading_spaces)
+    draw_title(draw_data, screen, tab, index, max_tab_length)
+    trailing_spaces = min(max_tab_length - 1, draw_data.trailing_spaces)
+    max_tab_length -= trailing_spaces
+    extra = screen.cursor.x - before - max_tab_length
+    if extra > 0:
+        screen.cursor.x -= extra + 1
+        screen.draw("…")
+    if trailing_spaces:
+        screen.draw(" " * trailing_spaces)
+    end = screen.cursor.x
+    screen.cursor.bold = screen.cursor.italic = False
+    screen.cursor.fg = 0
+    if not is_last:
+        screen.cursor.bg = as_rgb(color_as_int(draw_data.inactive_bg))
+    screen.cursor.bg = 0
+
+    # show keyboard mode on right side if set
+    if is_last:
+        mode = get_boss().mappings.current_keyboard_mode_name
+        if mode and not mode.startswith("__"):
+            screen.cursor.x = screen.columns - len(mode)
+            screen.cursor.fg = as_rgb(color_as_int(opts.color1))
+            screen.draw(mode)
+
+    return end
