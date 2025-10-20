b4d29059589ca8d9ca3051bbbd81156ea9262a2b
Will Norris <will@willnorris.com>
2025-10-19 20:49:31 -0700

    nvim.mini: start of mostly-minimal nvim experiment
    I'm still using plugins, just much fewer than usual, and trying out
    mini.nvim in place of a number of alternatives. I'll prrobably still
    pull in snacks.nvim at least.

---
+++ config/nvim.mini/README.md
@@ -0,0 +1,5 @@
+# nvim.mini
+
+This is an experiment to rebuild my neovim configuration in a more minimalist fashion.
+This means trying to lean in more on features recently added to core (like pack.add)
+and probably the mini.nvim set of plugins in place of many of my regulars.
---
+++ config/nvim.mini/init.lua
@@ -0,0 +1,22 @@
+vim.pack.add({
+  "https://github.com/nvim-mini/mini.nvim",
+})
+
+-- Set leader first, since some plugins use it
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("mini.basics").setup({
+  options = {
+    basic = true,
+    extra_ui = true,
+    win_border = "solid",
+  },
+  mappings = {
+    basic = true,
+    options_toggle_prefix = "<Leader>u",
+    windows = true,
+  },
+})
+require("mini.statusline").setup()
+require("mini.tabline").setup()
